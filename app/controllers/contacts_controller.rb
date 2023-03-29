e2e75e8915139dab524a05397ba67617efef7219
SAAD <SAADRAZI1998@GMAIL.com>
2023-03-29 11:59:51 -0500

    need some work

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,12 @@
+class ContactsController < ApplicationController
+  def show
+    @contact = Contact.find_by(id: 4)
+    render template: "contacts/show"
+  end 
+  
+  def all 
+    @contacts = Contact.all 
+    render template: "contact/index"
+  end 
+
+end
